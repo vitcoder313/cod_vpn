668e4dfc85787b189f217bf4ac0bb61e58091a06
Van Viet <viet.nguyen@urani.solutions>
2024-12-08 19:22:17 +0700

    fix css

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,13 @@ const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach((item) => {
 	item.querySelector('.faq-question').addEventListener('click', () => {
+		// Xóa class 'open' khỏi tất cả các item khác
+		faqItems.forEach((otherItem) => {
+			if (otherItem !== item) {
+				otherItem.classList.remove('open');
+			}
+		});
+		// Toggle class 'open' cho item hiện tại
 		item.classList.toggle('open');
 	});
 });
@@ -21,18 +28,18 @@ const updateHeroSectionBackground = () => {
 	if (currentMonth === 11) {
 		const heroSection = document.querySelector('.hero-section');
 		if (heroSection) {
-			// heroSection.classList.add('hero-section--winter')
-			heroSection.style.background = `
-      linear-gradient(89.92deg, rgba(0, 0, 0, 0.9) 18.8%, rgba(0, 0, 0, 0) 99.94%),
-      linear-gradient(180deg, rgba(0, 0, 0, 0.7) 2.72%, rgba(0, 0, 0, 0) 35.89%, rgba(0, 0, 0, 0.53) 100%),
-      url(assets/images/Snow.png),
-      url(assets/images/cod_vpn_gaming_promotion.jpeg)
-    `;
-			heroSection.style.backgroundSize = 'cover, contain, cover, cover';
-			heroSection.style.backgroundPosition =
-				'64% center, top left, 64% center, center';
-			heroSection.style.backgroundRepeat =
-				'no-repeat, no-repeat, no-repeat, no-repeat';
+			heroSection.classList.add('hero-section--winter')
+		// 	heroSection.style.background = `
+    //   linear-gradient(89.92deg, rgba(0, 0, 0, 0.9) 18.8%, rgba(0, 0, 0, 0) 99.94%),
+    //   linear-gradient(180deg, rgba(0, 0, 0, 0.7) 2.72%, rgba(0, 0, 0, 0) 35.89%, rgba(0, 0, 0, 0.53) 100%),
+    //   url(assets/images/Snow.png),
+    //   url(assets/images/cod_vpn_gaming_promotion.jpeg)
+    // `;
+		// 	heroSection.style.backgroundSize = 'cover, contain, cover, cover';
+		// 	heroSection.style.backgroundPosition =
+		// 		'64% center, top left, 64% center, center';
+		// 	heroSection.style.backgroundRepeat =
+		// 		'no-repeat, no-repeat, no-repeat, no-repeat';
 		}
 
 		const gameLeft = document.querySelector('.games-winter-left');
